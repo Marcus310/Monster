25f4122d35e919bdbfc63e6d209fdff120f654a2
Marcus Vinicíus <marcus1.apolinario@gmail.com>
2023-10-20 21:56:52 -0300

    subindo versão do projeto, com alterações de tamanho de fontes na tela de login, icone na tela de produtos inicial, para contato via WhatsApp, ,feedbacks não completos e footer também

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('whatsapp-button').addEventListener('mouseenter', function() {
+    this.style.transform = 'scale(1.1)';
+  });
+  
+  document.getElementById('whatsapp-button').addEventListener('mouseleave', function() {
+    this.style.transform = 'scale(1)';
+  });
+
+  
